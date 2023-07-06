277b879c2e6baa67732452c4f2461c52c37d0262
nearworld <artemismars2@outlook.com>
2023-07-06 22:06:46 +0900

    refactor: todo.list.tsx와 calendar.tsx에서 쓰이던 두 todo 배열 mock 데이터를 common폴더 내에 todoMockList로 통일

--- frontReactNative/src/screens/calendar.tsx
+++ frontReactNative/src/screens/calendar.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
 import {useState} from 'react';
 import {FlatList, View, StyleSheet, Dimensions} from 'react-native';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
+// import {Calendar} from 'components/Calendar/Calendar';
+import {
+  Calendar,
+  CalendarProvider,
+  LocaleConfig,
+  WeekCalendar,
+} from 'react-native-calendars';
 import {type RootBottomTabParamList} from '../../App';
 import {TodoType} from './todo.list';
 import ToDo from 'components/to.do';
 import CalendarModal from 'components/calendar.modal';
 import {useKeyboard} from 'hooks/useKeyboard';
 import TodoViewModal from 'components/todo.view.modal';
-import {DATE} from '../_common/get.date';
+import {todoMockList} from '_common/todo.mock.data';
 
 type CalendarProps = BottomTabScreenProps<RootBottomTabParamList, '달력'>;
 
@@ -17,161 +24,21 @@ const monthNames = Array.from({length: 12}).map(
   (item, index) => `${index + 1}월`,
 );
 const dayNames = ['월', '화', '수', '목', '금', '토', '일'];
-const apiTodoList: TodoType[] = [
-  {
-    id: '1',
-    date: 0,
-    todo: '가족들이랑 식사',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '2',
-    date: 0,
-    todo: '앱 개발',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '3',
-    date: 0,
-    todo: '미술관 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '4',
-    date: 0,
-    todo: '베트남 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '5',
-    date: 1685329441086,
-    todo: '학교 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '6',
-    date: 0,
-    todo: '학원 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '7',
-    date: 1685329441086,
-    todo: '월요일에 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '8',
-    date: 1685329441086,
-    todo: '화요일에 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '9',
-    date: 1685329441086,
-    todo: '수요일에 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '10',
-    date: 1685329441086,
-    todo: '지옥에 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-  {
-    id: '11',
-    date: 1685329441086,
-    todo: '수요일에 가기',
-    done: false,
-    authorId: '8654f7b1-d588-4c2b-87a3-124365f13cc1',
-    year: 2023,
-    month: 1,
-    day: 1,
-    createdAt: DATE,
-    updatedAt: DATE,
-    deletedAt: null,
-  },
-];
 
-LocaleConfig.locales.kr = {
-  monthNames,
-  monthNamesShort: monthNames,
-  dayNames,
-  dayNamesShort: dayNames,
-  today: '오늘',
-};
+// const monthNames = Array.from({length: 12}).map(
+//   (item, index) => `${index + 1}월`,
+// );
+// const dayNames = ['월', '화', '수', '목', '금', '토', '일'];
 
-LocaleConfig.defaultLocale = 'kr';
+// LocaleConfig.locales.kr = {
+//   monthNames,
+//   monthNamesShort: monthNames,
+//   dayNames,
+//   dayNamesShort: dayNames,
+//   today: '오늘',
+// };
+
+// LocaleConfig.defaultLocale = 'kr';
 
 const CalendarScreen: React.FC<CalendarProps> = () => {
   const [selected, setSelected] = useState('');
@@ -185,10 +52,9 @@ const CalendarScreen: React.FC<CalendarProps> = () => {
     <View
       style={[
         styles.container,
-        {
-          marginTop: isKeyboardVisible ? -keyboardHeight / 3 : 0,
-        },
+        {marginTop: isKeyboardVisible ? -keyboardHeight / 3 : 0},
       ]}>
+      {/* <Calendar /> */}
       <Calendar
         onDayPress={day => {
           setSelected(day.dateString);
@@ -204,7 +70,7 @@ const CalendarScreen: React.FC<CalendarProps> = () => {
       />
       <FlatList
         style={styles.to_do_list}
-        data={apiTodoList}
+        data={todoMockList}
         renderItem={({item}) => (
           <ToDo
             key={item.id}
